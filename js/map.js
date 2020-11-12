52f7f4e6f908ed289c5321e40515043a0064816c
erseco <erseco@users.noreply.github.com>
2020-11-12 10:53:42 +0000

    deploy: 491614200a4e05d3e44a75a82310bdf9cba7e085

---
+++ js/map.js
@@ -0,0 +1,8 @@
+//*************************** Theme Colours ****************************/
+// Define theme colour scheme
+var color1 = "#42A8C0";
+var color2 = "#4CAC9D";
+var color3 = "#5BB66F";
+var color4 = "#A15277";
+var color5 = "#FDA246";
+var color6 = "#4B6A78";
